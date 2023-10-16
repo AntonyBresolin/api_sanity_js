adf2e5df1c72a34b6965003def936cc9d5b5e30a
Lucas Dreveck <lucasdrevc@outlook.com>
2023-10-16 20:11:19 -0300

    rename pergunta frequente in index

--- schemas/index.js
+++ schemas/index.js
@@ -3,7 +3,7 @@ import avaliacao from './avaliacoes'
 import coordenacao from './coordenacao'
 import curso from './cursos'
 import noticia from './noticias'
-import pergunta from './perguntas-frequentes'
+import pergunta from './pergunta-frequente'
 import professor from './professores'
 
 export const schemaTypes = [avaliacao, coordenacao, curso, noticia, pergunta, professor]
