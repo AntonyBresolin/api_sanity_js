736fd8f5fddc13c9915c3cdad1a6f779318b1a96
Lucas Dreveck <lucasdrevc@outlook.com>
2023-11-06 19:37:07 -0300

    Remove email section
    remove email section because a security issue

--- schemas/index.js
+++ schemas/index.js
@@ -5,6 +5,5 @@ import curso from './cursos'
 import noticia from './noticias'
 import pergunta from './pergunta-frequente'
 import professor from './professores'
-import email from './emails'
 
-export const schemaTypes = [avaliacao, coordenacao, curso, noticia, pergunta, professor, email]
+export const schemaTypes = [avaliacao, coordenacao, curso, noticia, pergunta, professor]
